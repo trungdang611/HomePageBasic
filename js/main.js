0490b6aa5fba52dbcafaef28b4e33808eba2f91d
trungdang611 <trungdangdev06@gmai.com>
2025-01-15 23:50:39 +0700

    create close login modal & event

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 const modal = document.querySelector(".modal-section");
+const loginBtn = document.querySelector(".header-actions-login");
+const clostLoginModal = document.querySelector(".login-content-close");
 
-console.log(modal);
+loginBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+clostLoginModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+
+  //   console.log(event.target);
+};
